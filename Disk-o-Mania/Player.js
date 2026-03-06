7975949baf0e018207fcdefc1c4eec53cee88358
000noahknudsen000-boop <000noahknudsen000@gmail.com>
2026-03-06 15:33:45 +0100

    Player

--- Disk-o-Mania/Player.js
+++ Disk-o-Mania/Player.js
@@ -11,19 +11,25 @@ function PlayerSetup()
 function PlayerDraw()
 {
     //player movement
-    if (keyIsPressed && keyCode === UP_ARROW) {
-        xPlayer += cos(dirPlayer*0.1) * 5;
-        yPlayer += sin(dirPlayer*0.1) * 5;
+    if (keyIsDown(UP_ARROW)) {
+        xPlayer += cos(dirPlayer) * 5;
+        yPlayer += sin(dirPlayer) * 5;
         }
-    if (keyIsPressed && keyCode === DOWN_ARROW) {
-        xPlayer -= cos(dirPlayer*0.1) * 5;
-        yPlayer -= sin(dirPlayer*0.1) * 5;
+    if (keyIsDown(DOWN_ARROW)) {
+        xPlayer -= cos(dirPlayer) * 5;
+        yPlayer -= sin(dirPlayer) * 5;
+        }
+    if (keyIsDown(LEFT_ARROW)) {
+        dirPlayer -= 0.1;
+        }
+    if (keyIsDown(RIGHT_ARROW)) {
+        dirPlayer += 0.1;
         }
     //draw player
     push();
     fill(255,0,0);
     circle(xPlayer, yPlayer, rPlayer);
-    circle(xPlayer + cos(dirPlayer*0.1) * 10, yPlayer + sin(dirPlayer * 0.1) * 80, 10);
-    circle(xPlayer + cos(dirPlayer*0.1) * 10, yPlayer + sin(dirPlayer * 0.1) * -80, 10);
+    circle(xPlayer + cos(dirPlayer-PI/4)*10, yPlayer + sin(dirPlayer-PI/4)*10, 10);
+    circle(xPlayer + cos(dirPlayer+PI/4)*10, yPlayer + sin(dirPlayer+PI/4)*10, 10);
     pop();
 }
