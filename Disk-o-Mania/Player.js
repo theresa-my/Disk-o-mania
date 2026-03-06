0c2d92fd9f7a3e0d7ba1bcb53533755a046add6b
000noahknudsen000-boop <000noahknudsen000@gmail.com>
2026-03-06 15:18:04 +0100

    player

--- Disk-o-Mania/Player.js
+++ Disk-o-Mania/Player.js
@@ -1,11 +1,29 @@
-PlayerSetup()
+function PlayerSetup()
 {
-xplayer = 0;    
-yplayer = 50;
+    xPlayer = 0;    
+    yPlayer = 50;
+    rPlayer = 20;
+    dirPlayer = 1;
 
 }
 
-PlayerDraw()
+
+function PlayerDraw()
 {
-circle(xplayer, yplayer, 20);
+    //player movement
+    if (keyIsPressed && keyCode === UP_ARROW) {
+        xPlayer += cos(dirPlayer*0.1) * 5;
+        yPlayer += sin(dirPlayer*0.1) * 5;
+        }
+    if (keyIsPressed && keyCode === DOWN_ARROW) {
+        xPlayer -= cos(dirPlayer*0.1) * 5;
+        yPlayer -= sin(dirPlayer*0.1) * 5;
+        }
+    //draw player
+    push();
+    fill(255,0,0);
+    circle(xPlayer, yPlayer, rPlayer);
+    circle(xPlayer + cos(dirPlayer*0.1) * 10, yPlayer + sin(dirPlayer * 0.1) * 80, 10);
+    circle(xPlayer + cos(dirPlayer*0.1) * 10, yPlayer + sin(dirPlayer * 0.1) * -80, 10);
+    pop();
 }
