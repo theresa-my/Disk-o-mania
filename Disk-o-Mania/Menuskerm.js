4c20a4f75ae829bb7af6a1fb95c6885879c25067
000noahknudsen000-boop <000noahknudsen000@gmail.com>
2026-03-26 09:11:01 +0100

    s

--- Disk-o-Mania/Menuskerm.js
+++ Disk-o-Mania/Menuskerm.js
@@ -1,14 +1,32 @@
 function setupMenu()
 {
-
+transitiontimer = 0;
 
 }
 
 function drawMenu()
 {
+    transitiontimer++;
 
-state = "spil"
-text ("Menu", 100, 100);
-level = "Iss"
+if (keyIsPressed && transitiontimer > 30)
+{
+    state = "spil";
+}
+    
+push();
+    background(0);
+    fill(255,0,0);
+    textSize(110);
+    textFont("Impact");
+    text ("Disk-o-Mania", 110, 230);
+pop();  
+
+push();
+    fill(255,0,0);
+    textSize(30);
+    textFont("Impact");
+    text ("Click to start", 350, 400);
+pop();
+    level = "Iss"
 
 }
