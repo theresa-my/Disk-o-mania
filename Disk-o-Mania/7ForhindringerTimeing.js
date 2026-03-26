1bb0ea3ec10d8146cbdeb7c81b53b5414f65a965
000noahknudsen000-boop <000noahknudsen000@gmail.com>
2026-03-26 10:13:52 +0100

    s

--- Disk-o-Mania/7ForhindringerTimeing.js
+++ Disk-o-Mania/7ForhindringerTimeing.js
@@ -1,15 +1,25 @@
 function ForhindringerTimeingSetup()
 {
     Forhindringerbolde = []
+
+    for (let i = 0; i < 10; i++)
+    {
+        i = new BoldeForhindring(0,1)
+
+    }
 }
 
 function ForhindringerTimeingDraw()
 {
+//Firkant forhindringer
   for (let i = 0; i < forhindringer.length; i++)
   {
     forhindringer[i].roter(0, 100, 30, 60);
   }
 
-
-
+//Bolde forhindringer
+  for (let i = 0; i < Forhindringerbolde.length; i++)
+  {
+    Forhindringerbolde[i].SkydBoldeforhindring();
+  }
 }
